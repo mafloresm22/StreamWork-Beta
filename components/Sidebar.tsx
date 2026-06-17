2e51f6aa9e67e39b0fd815c5085c9f8a220e1118
mafloresm22 <mathiasfabricio22f@gmail.com>
2026-06-17 13:00:30 -0500

    feat: initialize project layout with Navbar, Sidebar, and global CSS theme configuration

---
+++ components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between border-r border-border bg-card p-6 pt-28 text-card-foreground max-sm:hidden lg:w-[264px]'>
+            <div className='flex flex-col gap-6'>
+                {/* Aquí irán los enlaces de navegación */}
+            </div>
+        </section>
+    )
+}
+
+export default Sidebar
